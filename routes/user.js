dd61318b7706480f01794150cd820799b6406980
@ahmedyazid.mejri <ahmedyazid.mejri@esprit.tn>
2023-10-14 17:58:17 +0100

    adding validator

--- routes/user.js
+++ routes/user.js
@@ -6,10 +6,11 @@ const {
   deleteUser,
   updateUser,
 } = require("../controllers/userController");
+const { uservalidator, validate } = require("../middlewares/validators");
 
 const router = express.Router();
 
-router.post("/add", createuser);
+router.post("/add", uservalidator, validate, createuser);
 router.get("/all", getUsers);
 router.get("/:id", getUser);
 router.delete("/:id", deleteUser);
