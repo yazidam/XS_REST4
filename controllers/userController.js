7f8ccd7c818d2f9ee7f5f022037199b8aee8d5ac
@ahmedyazid.mejri <ahmedyazid.mejri@esprit.tn>
2023-10-14 16:55:46 +0100

    adding hash password and update func

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,13 @@
 const User = require("../models/user");
-
+const bcrypt = require("bcrypt");
 const createuser = async (req, res) => {
   try {
     const user = new User(req.body);
-    const { email } = req.body;
+    const { email, password } = req.body;
     const exist = await User.findOne({ email });
     if (exist) res.status(400).json("user with this eamil existe");
+    const hashPsw = await bcrypt.hash(password, 10);
+    user.password = hashPsw;
     const newUser = await user.save();
     return res.status(201).json(newUser);
   } catch (error) {
@@ -47,9 +49,33 @@ const deleteUser = async (req, res) => {
   }
 };
 
+const updateUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { email, name, phone } = req.body;
+
+    console.log("user", user);
+    if (user) {
+      user.name = name || user.name;
+      user.email = email || user.email;
+      user.phone = phone || user.phone;
+      const updateUser = await user.save();
+      res.json({
+        updateUser,
+      });
+      console.log("updateUser", updateUser);
+    }
+    await user.save();
+  } catch (error) {
+    console.log(error);
+    res.status(404).send({ error: "user not found try again" });
+  }
+};
+
 module.exports = {
   createuser,
   getUsers,
   getUser,
   deleteUser,
+  updateUser,
 };
